07c54abbb74cb11aa70b16d8e748367398ace934
anand <anand9522s@gmail.com>
2018-01-05 17:55:25 +0100

    New

--- app/controllers/work_portfolios_controller.rb
+++ app/controllers/work_portfolios_controller.rb
@@ -2,4 +2,20 @@ class WorkPortfoliosController < ApplicationController
   def index
     @portfolios = WorkPortfolio.all
   end
+
+  def new
+    @portfolio_item = WorkPortfolio.new
+  end
+
+  def create
+    @portfolio_item = WorkPortfolio.new(params.require(:work_portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to work_portfolios_path, notice: 'Kaam ho gaya. Paise mil jaane chahiye :D'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
